e3f708d44e8a197db635edf2bf84c8ee595354b5
SsoSan <s3sd1004@naver.com>
2025-03-07 16:21:22 +0900

    Number

---
+++ RealNumberPractice.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RealNumberPractice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float f = 3.14f;
+        double d = 3.14;
+        decimal m = 3.14m;
+
+
+        Debug.Log("float: " + f);
+        Debug.Log("double: " + d);
+        Debug.Log("decimal: " + m);
+    }
+}
+
+
+/*
+[Q]
+½Ç¼öÇü °ª float 3.14, double 3.14, decimal 3.14 °ª Ãâ·ÂÇÏ±â
+
+[output]
+float: 3.14
+double: 3.14
+decimal: 3.14
+*/
